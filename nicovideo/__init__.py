432f74d355b8beb7c1d0fac078e3f8add91994fc
okaits#7534 <okaits@okaits7534.mydns.jp>
2023-09-02 04:18:41 +0900

    型ヒント大強化など
    * 不要なImportを整理
    * Union[Any, type(None)] → Optional[Any]
    * Video.get_metadataをVideoから出す
    * soup.find → soup.select
    * 一部間違っている型ヒントを修正
    * get_metadataの引数にreturn_rawdictを追加。これがFalseだと今までで言うdataの内容がそのまま返却される。Trueでは従来通りのデータが返される。
    * get_metadataでreturn_rawdictがTrueの時に返却される辞書の型ヒントにTypedDictを使う
    * mypy.iniを作る

--- nicovideo/__init__.py
+++ nicovideo/__init__.py
@@ -2,12 +2,10 @@
 from __future__ import annotations
 
 import datetime
-import pprint
 import urllib.error
 import urllib.request
 from functools import cache
-from html import unescape
-from typing import Type, Union
+from typing import Literal, Optional, TypedDict, Final, overload
 from dataclasses import dataclass
 
 import json5
@@ -29,10 +27,9 @@ def _urllib_request_with_cache(url: str) -> str:
     with urllib.request.urlopen(url) as response:
         return response.read()
 
-@dataclass
+
 class Video():
-    """ Video """
-    videoid: str
+    """ Video Classes/Methods """
 
     @dataclass
     class Metadata(): # pylint: disable=R0902
@@ -44,14 +41,14 @@ class Video():
         counts     : Video.Metadata.Counts
         duration   : int
         postdate   : datetime.datetime
-        genre      : Union[Video.Metadata.Genre, type(None)]
+        genre      : Optional[Video.Metadata.Genre]
         tags       : list[Video.Metadata.Tag]
         ranking    : Video.Metadata.Ranking
-        series     : Video.Metadata.Series
+        series     : Optional[Video.Metadata.Series]
         thumbnail  : Video.Metadata.Thumbnail
 
         def __post_init__(self):
-            self.url: str = f'https://www.nicovideo.jp/watch/{self.videoid}'
+            self.url: Final[str] = f'https://www.nicovideo.jp/watch/{self.videoid}'
 
         @dataclass
         class User():
@@ -82,8 +79,8 @@ class Video():
         @dataclass
         class Ranking():
             """ Ranking data """
-            genreranking: Union[Video.Metadata.Ranking.Genre, None]
-            tagrankings : list[Video.Metadata.Ranking.Genre]
+            genreranking: Optional[Video.Metadata.Ranking.Genre]
+            tagrankings : list[Video.Metadata.Ranking.Tag]
 
             @dataclass
             class Genre():
@@ -105,115 +102,133 @@ class Video():
             title      : str
             description: str
             thumbnail  : str
-            prev_video : Union[Video, type(None)]
-            next_video : Union[Video, type(None)]
-            first_video: Union[Video, type(None)]
+            prev_video : Optional[str]
+            next_video : Optional[str]
+            first_video: Optional[str]
 
         @dataclass
         class Thumbnail():
             """ Thumbnail data """
-            small_url : Union[str, type(None)]
-            middle_url: Union[str, type(None)]
-            large_url : Union[str, type(None)]
-            player_url: Union[str, type(None)]
-            ogp_url   : Union[str, type(None)]
+            small_url : Optional[str]
+            middle_url: Optional[str]
+            large_url : Optional[str]
+            player_url: Optional[str]
+            ogp_url   : Optional[str]
 
-    def get_metadata(self, use_cache: bool = False) -> dict[str, Union[Video.Metadata, dict]]:
-        """ Get video's metadata """
-        watch_url = f"https://www.nicovideo.jp/watch/{self.videoid}"
-        try:
-            if use_cache:
-                text = _urllib_request_with_cache(watch_url)
-            else:
-                with urllib.request.urlopen(watch_url) as response:
-                    text = response.read()
-        except urllib.error.HTTPError as exc:
-            if exc.code == 404:
-                raise Error.NicovideoClientError.VideoNotFound("Video not found or deleted.")\
-                    from exc
-            else:
-                raise Error.NicovideoClientError.ConnectionError(
-                    f"Unexpected HTTP Error: {exc.code}"
-                ) from exc
-        except urllib.error.URLError as exc:
-            raise Error.NicovideoClientError.ConnectionError("Connection error.") from exc
+class _ReturnDataWithRawdict(TypedDict):
+    data   : Video.Metadata
+    rawdict: dict
 
-        soup = bs(text, "html.parser")
-        rawdict = json5.loads(
-            str(soup.find("div", id="js-initial-watch-data")["data-api-data"])
-        )
+@overload
+def get_metadata(videoid: str, *, use_cache: bool = False) -> Video.Metadata: ...
 
-        # Tags
-        tags = []
-        for tag in rawdict['tag']['items']:
-            tags.append(
-                Video.Metadata.Tag(
-                    name   = tag['name'],
-                    locked = tag['isLocked']
-                )
+@overload
+def get_metadata(videoid: str, *, return_rawdict: Literal[False], use_cache: bool = False) \
+        -> Video.Metadata: ...
+
+@overload
+def get_metadata(videoid: str, *, return_rawdict: Literal[True], use_cache: bool = False) \
+        -> _ReturnDataWithRawdict: ...
+
+def get_metadata(videoid: str, *, return_rawdict: bool = False, use_cache: bool = False): #pylint: disable=C0301
+    """ Get video's metadata """
+    watch_url = f"https://www.nicovideo.jp/watch/{videoid}"
+    try:
+        if use_cache:
+            text = _urllib_request_with_cache(watch_url)
+        else:
+            with urllib.request.urlopen(watch_url) as response:
+                text = response.read()
+    except urllib.error.HTTPError as exc:
+        if exc.code == 404:
+            raise Error.NicovideoClientError.VideoNotFound("Video not found or deleted.")\
+                from exc
+        else:
+            raise Error.NicovideoClientError.ConnectionError(
+                f"Unexpected HTTP Error: {exc.code}"
+            ) from exc
+    except urllib.error.URLError as exc:
+        raise Error.NicovideoClientError.ConnectionError("Connection error.") from exc
+
+    soup = bs(text, "html.parser")
+    rawdict = json5.loads(
+        str(soup.select("#js-initial-watch-data")[0]["data-api-data"])
+    )
+
+    # Tags
+    tags = []
+    for tag in rawdict['tag']['items']:
+        tags.append(
+            Video.Metadata.Tag(
+                name   = tag['name'],
+                locked = tag['isLocked']
             )
-
-        # Ranking
-        ranking_tags = []
-        for ranking_tag in rawdict['ranking']['popularTag']:
-            for tag in tags:
-                if tag.name == ranking_tag['tag']:
-                    ranking_tags.append(
-                        Video.Metadata.Ranking.Tag(
-                            tag,
-                            ranking_tag['rank'],
-                            datetime.datetime.fromisoformat(ranking_tag['dateTime'])
-                        )
-                    )
-                    break
-        ranking_genre = Video.Metadata.Ranking.Genre(
-            rawdict['ranking']['genre']['genre'],
-            rawdict['ranking']['genre']['rank'] ,
-            datetime.datetime.fromisoformat(rawdict['ranking']['genre']['dateTime'])
-        ) if rawdict['ranking']['genre'] else None
-
-        data = self.Metadata(
-            videoid     = rawdict['video']['id'],
-            title       = rawdict['video']['title'],
-            description = rawdict['video']['description'],
-            owner       = Video.Metadata.User(
-                           nickname = rawdict['owner']['nickname'],
-                           userid   = rawdict['owner']['id']
-                          ),
-            counts      = self.Metadata.Counts(
-                           comments = rawdict['video']['count']['comment'],
-                           likes    = rawdict['video']['count']['like'],
-                           mylists  = rawdict['video']['count']['mylist'],
-                           views    = rawdict['video']['count']['view']
-                          ),
-            duration    = rawdict['video']['duration'],
-            postdate    = datetime.datetime.fromisoformat(
-                           rawdict['video']['registeredAt']
-                          ),
-            genre       = Video.Metadata.Genre(
-                           label    = rawdict['genre']['label'],
-                           key      = rawdict['genre']['key']
-                          ),
-            ranking     = Video.Metadata.Ranking(ranking_genre, ranking_tags),
-            series      = Video.Metadata.Series(
-                           seriesid    = rawdict['series']['id'],
-                           title       = rawdict['series']['title'],
-                           description = rawdict['series']['description'],
-                           thumbnail   = rawdict['series']['thumbnailUrl'],
-                           prev_video  = Video(rawdict['series']['video']['prev']['id'])
-                               if rawdict['series']['video']['prev'] else None,
-                           next_video  = Video(rawdict['series']['video']['next']['id'])
-                               if rawdict['series']['video']['next'] else None,
-                           first_video = Video(rawdict['series']['video']['first']['id'])
-                               if rawdict['series']['video']['first'] else None
-               ) if rawdict['series'] else None,
-            thumbnail   = Video.Metadata.Thumbnail(
-                           small_url  = rawdict['video']['thumbnail']['url'],
-                           middle_url = rawdict['video']['thumbnail']['middleUrl'],
-                           large_url  = rawdict['video']['thumbnail']['largeUrl'],
-                           player_url = rawdict['video']['thumbnail']['player'],
-                           ogp_url    = rawdict['video']['thumbnail']['ogp']
-                ),
-            tags        = tags
         )
-        return {"data": data, "rawdict": rawdict}
+
+    # Ranking
+    ranking_tags = []
+    for ranking_tag in rawdict['ranking']['popularTag']:
+        for tag in tags:
+            if tag.name == ranking_tag['tag']:
+                ranking_tags.append(
+                    Video.Metadata.Ranking.Tag(
+                        tag,
+                        ranking_tag['rank'],
+                        datetime.datetime.fromisoformat(ranking_tag['dateTime'])
+                    )
+                )
+                break
+    ranking_genre = Video.Metadata.Ranking.Genre(
+        rawdict['ranking']['genre']['genre'],
+        rawdict['ranking']['genre']['rank'] ,
+        datetime.datetime.fromisoformat(rawdict['ranking']['genre']['dateTime'])
+    ) if rawdict['ranking']['genre'] else None
+
+    data = Video.Metadata(
+        videoid     = rawdict['video']['id'],
+        title       = rawdict['video']['title'],
+        description = rawdict['video']['description'],
+        owner       = Video.Metadata.User(
+                        nickname = rawdict['owner']['nickname'],
+                        userid   = rawdict['owner']['id']
+                        ),
+        counts      = Video.Metadata.Counts(
+                        comments = rawdict['video']['count']['comment'],
+                        likes    = rawdict['video']['count']['like'],
+                        mylists  = rawdict['video']['count']['mylist'],
+                        views    = rawdict['video']['count']['view']
+                        ),
+        duration    = rawdict['video']['duration'],
+        postdate    = datetime.datetime.fromisoformat(
+                        rawdict['video']['registeredAt']
+                        ),
+        genre       = Video.Metadata.Genre(
+                        label    = rawdict['genre']['label'],
+                        key      = rawdict['genre']['key']
+                        ),
+        ranking     = Video.Metadata.Ranking(ranking_genre, ranking_tags),
+        series      = Video.Metadata.Series(
+                        seriesid    = rawdict['series']['id'],
+                        title       = rawdict['series']['title'],
+                        description = rawdict['series']['description'],
+                        thumbnail   = rawdict['series']['thumbnailUrl'],
+                        prev_video  = rawdict['series']['video']['prev']['id']
+                            if rawdict['series']['video']['prev'] else None,
+                        next_video  = rawdict['series']['video']['next']['id']
+                            if rawdict['series']['video']['next'] else None,
+                        first_video = rawdict['series']['video']['first']['id']
+                            if rawdict['series']['video']['first'] else None
+            ) if rawdict['series'] else None,
+        thumbnail   = Video.Metadata.Thumbnail(
+                        small_url  = rawdict['video']['thumbnail']['url'],
+                        middle_url = rawdict['video']['thumbnail']['middleUrl'],
+                        large_url  = rawdict['video']['thumbnail']['largeUrl'],
+                        player_url = rawdict['video']['thumbnail']['player'],
+                        ogp_url    = rawdict['video']['thumbnail']['ogp']
+            ),
+        tags        = tags
+    )
+    if return_rawdict:
+        return _ReturnDataWithRawdict(data=data, rawdict=rawdict)
+    else:
+        return data
