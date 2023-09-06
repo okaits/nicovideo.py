54172b0ca0838cacf7a3a29e89ed42f7249f4619
okaits#7534 <okaits@okaits7534.net>
2023-09-06 23:19:54 +0900

    Userクラス追加＆get_metadataの位置変更
    なんか最近get_metadataが移動しまくってる気がする

--- README.md
+++ README.md
@@ -15,8 +15,17 @@ python3 -m pip install -r requirements.txt
 
 ```python3
 import nicovideo
-video = nicovideo.Video('動画ID')
-metadata = video.get_metadata()['data']
+
+# 動画: sm9（新・豪血寺一族 -煩悩解放 - レッツゴー！陰陽師）
+sm9 = nicovideo.Video.get_metadata("sm9")
+print(f"タイトル: {sm9.title}")
+print(f"再生数: {sm9.counts.views}")
+
+# ユーザー: user/9003560（くりたしげたか）
+kurita = nicovideo.User.get_metadata(9003560)
+print(f"ニックネーム: {kurita.nickname}")
+print(f"ユーザーレベル: {kurita.user_level}")
+
 ```
 
 ## クラス・関数やその返り値など
@@ -24,26 +33,16 @@ metadata = video.get_metadata()['data']
 `class クラス名(初期化時の引数: 型ヒント = デフォルト値, ...)`  
 `def   関数名(引数: 型ヒント = デフォルト値, ...) -> 返り値型ヒント`
 
-### `def Video.get_metadata(videoid: str, *, return_rawdict: bool = False, use_cache: bool = False) -> ...`
-動画のメタデータを取得する関数です。  
+### `class Video()`
+動画のクラスです。
+
+#### `(classmethod) def Video.get_metadata(videoid: str, *, use_cache: bool = False)`
+動画のメタデータを取得するメソッドです。  
   
-返り値:
-```python3
-# return_rawdictがTrueの場合:
-{
-    "data": 指定された動画の動画情報のパース結果（Video.Metadata）,
-    "rawdict": パース前のウェブサイトから直接取得したデータ（dict）
-}
-
-# return_rawdictが未指定/Falseの場合:
-指定された動画の動画情報のパース結果（Video.Metadata）
-```
-
-### `class Video(videoid: str = 動画ID)`
-動画のクラスです。このクラスはネストされた物を含めてすべて`get_metadata()`が管理するので、基本的には手動で初期化することはありません。  
+返り値: `Video.Metadata`
 
 #### `class Video.Metadata(...)`
-動画のメタデータを格納するクラスです。`get_metadata()`の返り値に含まれます。   
+動画のメタデータを格納するクラスです。`get_metadata()`の返り値です。   
 
 インスタンス変数一覧:
 ```
@@ -60,10 +59,11 @@ ranking    : Video.Metadata.Ranking          = ランキングデータ
 series     : Optional[Video.Metadata.Series] = シリーズ
 thumbnail  : Video.Metadata.Thumbnail        = サムネイル
 url        : str                             = 視聴URL
+rawdict    : dict                            = サーバーから取得した加工前生データ（デバッグ用）
 ```
 
 ##### `class Video.Metadata.User(...)`
-ユーザーのクラスです。投稿者などを表します。  
+ユーザーのクラスです。投稿者などを表します。（`User`クラスの簡易版です。）
   
 インスタンス変数一覧:
 ```
@@ -128,7 +128,7 @@ rank: int                = ランキング最高順位
 time: datetime.datetime  = 順位獲得日時
 ```
 
-##### `Class Video.Metadata.Series(...)`
+##### `class Video.Metadata.Series(...)`
 シリーズのクラスです。  
   
 ```
@@ -141,7 +141,7 @@ next_video : Union[Video, NoneType] = 次動画
 first_video: Union[Video, NoneType] = 最初の動画
 ```
 
-##### `Class Video.Metadata.Thumbnail(...)`
+##### `class Video.Metadata.Thumbnail(...)`
 サムネイル画像のクラスです。  
   
 ```
@@ -151,6 +151,86 @@ large_url : str = サムネイル（大）URL
 player_url: str = サムネイル（プレイヤー用）URL
 ogp_url   : str = サムネイル（OGP表示用）URL
 ```
+
+### `class User()`
+ユーザーのクラスです。
+
+#### `(classmethod) def User.get_metadata(userid: int, *, use_cache: bool = False)`
+ユーザーのメタデータを取得するメソッドです。  
+  
+返り値: `User.Metadata`
+
+#### `class User.Metadata(...)`
+動画のメタデータを格納するクラスです。`get_metadata()`の返り値です。   
+
+インスタンス変数一覧:
+```python3
+nickname          : str                           = ユーザーニックネーム
+userid            : int                           = ユーザーID
+description       : User.Metadata.Description     = ユーザー説明欄（bio）
+user_type         : Literal["Premium", "General"] = ユーザータイプ（Premium/General）
+registered_version: str                           = 登録時バージョン
+follow            : int                           = フォロー数
+follower          : int                           = フォロワー数
+user_level        : int                           = ユーザーレベル
+user_exp          : int                           = ユーザーEXP
+sns               : list[User.Metadata.SNS.User]  = SNS連携情報
+cover             : Optional[User.Metadata.Cover] = カバー画像
+icon              : User.Metadata.UserIcon        = アイコン画像
+rawdict           : dict                          = サーバーから取得した加工前生データ（デバッグ用）
+```
+
+##### `class User.Description(...)`
+ユーザーの説明文(bio)です。  
+  
+インスタンス変数一覧:
+```python3
+description_html : str = 説明欄（text/html）
+description_plain: str = 説明欄（text/plain）
+```
+
+##### `class User.SNS(...)`
+ユーザーのプロフィールに載ってるSNSについてのクラスです。
+
+###### `class User.SNS.Service(...)`
+SNSサービスの名称とかアイコンとかです。  
+  
+インスタンス変数一覧:
+```python3
+name: str = SNSサービス名称
+key : str = SNSのタイプ
+icon: str = SNSのロゴ（PNGファイルのURL）
+```
+
+###### `class User.SNS.User(...)`
+SNSユーザーについてのクラスです。  
+  
+インスタンス変数一覧:
+```python3
+service: User.Metadata.SNS.Service = SNSサービス
+name   : str                       = SNSユーザ＝名
+url    : str                       = SNSプロフィールURL
+```
+
+##### `class User.Cover(...)`
+ユーザーのカバー画像についてのクラスです。  
+  
+インスタンス変数一覧:
+```python3
+ogp: str = OGP用カバー画像URL
+pc : str = PCサイズカバー画像URL
+sp : str = SP（スマートフォン）サイズカバー画像
+```
+
+##### `class User.UserIcon(...)`
+ユーザーアイコンについてのクラスです。  
+  
+インスタンス変数一覧:
+```python3
+small: str = ユーザーアイコン（小）URL
+large: str = ユーザーアイコン（大）URL
+```
+
 # License
 適用ライセンス: LGPL 3.0  
 Copyright © 2023 okaits#7534
--- nicovideo/__init__.py
+++ nicovideo/__init__.py
@@ -5,7 +5,7 @@ import datetime
 import urllib.error
 import urllib.request
 from functools import cache
-from typing import Literal, Optional, TypedDict, Final, Annotated, TypeAlias, overload
+from typing import Literal, Optional, Final, Annotated
 from dataclasses import dataclass
 import re
 
@@ -14,16 +14,11 @@ from bs4 import BeautifulSoup as bs
 
 __version__ = '2.0.0'
 
-VIDEOID: TypeAlias = Annotated[str, re.compile(
-    "^sm.*$|^nm.*$|^so.*$|^ax.*$|^ca.*$|^cd.*$|^cw.*$|^fx.*$|^ig.*$|^na.*$|^om.*$|^sd.*$"\
-    "|^sk.*$|^yk.*$|^yo.*$|^za.*$|^zb.*$|^zc.*$|^zd.*$|^ze.*$|^nl.*$"
-)]
-
 class Error():
     """ Errors """
     class NicovideoClientError(Exception):
         """ urllib error """
-        class VideoNotFound(Exception):
+        class ContentNotFound(Exception):
             """ Video not found or deleted """
         class ConnectionError(Exception):
             """ Connection error """
@@ -40,7 +35,7 @@ class Video():
     @dataclass
     class Metadata(): # pylint: disable=R0902
         """ Meta data """
-        videoid    : VIDEOID
+        videoid    : str
         title      : str
         description: str
         owner      : Video.Metadata.User
@@ -52,6 +47,7 @@ class Video():
         ranking    : Video.Metadata.Ranking
         series     : Optional[Video.Metadata.Series]
         thumbnail  : Video.Metadata.Thumbnail
+        rawdict    : dict
 
         def __post_init__(self):
             self.url: Final[str] = f'https://www.nicovideo.jp/watch/{self.videoid}'
@@ -121,120 +117,228 @@ class Video():
             player_url: Optional[str]
             ogp_url   : Optional[str]
 
-class _ReturnDataWithRawdict(TypedDict):
-    data   : Video.Metadata
-    rawdict: dict
+    @staticmethod
+    def get_metadata(videoid: str, *, use_cache: bool = False): #pylint: disable=C0301
+        """ Get video's metadata """
+        watch_url = f"https://www.nicovideo.jp/watch/{videoid}"
+        try:
+            if use_cache:
+                text = _urllib_request_with_cache(watch_url)
+            else:
+                with urllib.request.urlopen(watch_url) as response:
+                    text = response.read()
+        except urllib.error.HTTPError as exc:
+            if exc.code == 404:
+                raise Error.NicovideoClientError.ContentNotFound("Video not found or deleted.")\
+                    from exc
+            else:
+                raise Error.NicovideoClientError.ConnectionError(
+                    f"Unexpected HTTP Error: {exc.code}"
+                ) from exc
+        except urllib.error.URLError as exc:
+            raise Error.NicovideoClientError.ConnectionError("Connection error.") from exc
 
-@overload
-def get_metadata(videoid: VIDEOID, *, use_cache: bool = False) -> Video.Metadata: ...
-
-@overload
-def get_metadata(videoid: VIDEOID, *, return_rawdict: Literal[False], use_cache: bool = False) \
-        -> Video.Metadata: ...
-
-@overload
-def get_metadata(videoid: VIDEOID, *, return_rawdict: Literal[True], use_cache: bool = False) \
-        -> _ReturnDataWithRawdict: ...
-
-def get_metadata(videoid: VIDEOID, *, return_rawdict: bool = False, use_cache: bool = False): #pylint: disable=C0301
-    """ Get content's metadata """
-    watch_url = f"https://www.nicovideo.jp/watch/{videoid}"
-    try:
-        if use_cache:
-            text = _urllib_request_with_cache(watch_url)
-        else:
-            with urllib.request.urlopen(watch_url) as response:
-                text = response.read()
-    except urllib.error.HTTPError as exc:
-        if exc.code == 404:
-            raise Error.NicovideoClientError.VideoNotFound("Video not found or deleted.")\
-                from exc
-        else:
-            raise Error.NicovideoClientError.ConnectionError(
-                f"Unexpected HTTP Error: {exc.code}"
-            ) from exc
-    except urllib.error.URLError as exc:
-        raise Error.NicovideoClientError.ConnectionError("Connection error.") from exc
-
-    soup = bs(text, "html.parser")
-    rawdict = json5.loads(
-        str(soup.select("#js-initial-watch-data")[0]["data-api-data"])
-    )
-
-    # Tags
-    tags = []
-    for tag in rawdict['tag']['items']:
-        tags.append(
-            Video.Metadata.Tag(
-                name   = tag['name'],
-                locked = tag['isLocked']
-            )
+        soup = bs(text, "html.parser")
+        rawdict = json5.loads(
+            str(soup.select("#js-initial-watch-data")[0]["data-api-data"])
         )
 
-    # Ranking
-    ranking_tags = []
-    for ranking_tag in rawdict['ranking']['popularTag']:
-        for tag in tags:
-            if tag.name == ranking_tag['tag']:
-                ranking_tags.append(
-                    Video.Metadata.Ranking.Tag(
-                        tag,
-                        ranking_tag['rank'],
-                        datetime.datetime.fromisoformat(ranking_tag['dateTime'])
-                    )
+        # Tags
+        tags = []
+        for tag in rawdict['tag']['items']:
+            tags.append(
+                Video.Metadata.Tag(
+                    name   = tag['name'],
+                    locked = tag['isLocked']
                 )
-                break
-    ranking_genre = Video.Metadata.Ranking.Genre(
-        rawdict['ranking']['genre']['genre'],
-        rawdict['ranking']['genre']['rank'] ,
-        datetime.datetime.fromisoformat(rawdict['ranking']['genre']['dateTime'])
-    ) if rawdict['ranking']['genre'] else None
+            )
 
-    data = Video.Metadata(
-        videoid     = rawdict['video']['id'],
-        title       = rawdict['video']['title'],
-        description = rawdict['video']['description'],
-        owner       = Video.Metadata.User(
-                        nickname = rawdict['owner']['nickname'],
-                        userid   = rawdict['owner']['id']
-                        ),
-        counts      = Video.Metadata.Counts(
-                        comments = rawdict['video']['count']['comment'],
-                        likes    = rawdict['video']['count']['like'],
-                        mylists  = rawdict['video']['count']['mylist'],
-                        views    = rawdict['video']['count']['view']
-                        ),
-        duration    = rawdict['video']['duration'],
-        postdate    = datetime.datetime.fromisoformat(
-                        rawdict['video']['registeredAt']
-                        ),
-        genre       = Video.Metadata.Genre(
-                        label    = rawdict['genre']['label'],
-                        key      = rawdict['genre']['key']
-                        ),
-        ranking     = Video.Metadata.Ranking(ranking_genre, ranking_tags),
-        series      = Video.Metadata.Series(
-                        seriesid    = rawdict['series']['id'],
-                        title       = rawdict['series']['title'],
-                        description = rawdict['series']['description'],
-                        thumbnail   = rawdict['series']['thumbnailUrl'],
-                        prev_video  = rawdict['series']['video']['prev']['id']
-                            if rawdict['series']['video']['prev'] else None,
-                        next_video  = rawdict['series']['video']['next']['id']
-                            if rawdict['series']['video']['next'] else None,
-                        first_video = rawdict['series']['video']['first']['id']
-                            if rawdict['series']['video']['first'] else None
-            ) if rawdict['series'] else None,
-        thumbnail   = Video.Metadata.Thumbnail(
-                        small_url  = rawdict['video']['thumbnail']['url'],
-                        middle_url = rawdict['video']['thumbnail']['middleUrl'],
-                        large_url  = rawdict['video']['thumbnail']['largeUrl'],
-                        player_url = rawdict['video']['thumbnail']['player'],
-                        ogp_url    = rawdict['video']['thumbnail']['ogp']
+        # Ranking
+        ranking_tags = []
+        for ranking_tag in rawdict['ranking']['popularTag']:
+            for tag in tags:
+                if tag.name == ranking_tag['tag']:
+                    ranking_tags.append(
+                        Video.Metadata.Ranking.Tag(
+                            tag,
+                            ranking_tag['rank'],
+                            datetime.datetime.fromisoformat(ranking_tag['dateTime'])
+                        )
+                    )
+                    break
+        ranking_genre = Video.Metadata.Ranking.Genre(
+            rawdict['ranking']['genre']['genre'],
+            rawdict['ranking']['genre']['rank'] ,
+            datetime.datetime.fromisoformat(rawdict['ranking']['genre']['dateTime'])
+        ) if rawdict['ranking']['genre'] else None
+
+        return Video.Metadata(
+            videoid     = rawdict['video']['id'],
+            title       = rawdict['video']['title'],
+            description = rawdict['video']['description'],
+            owner       = Video.Metadata.User(
+                            nickname = rawdict['owner']['nickname'],
+                            userid   = rawdict['owner']['id']
+                            ),
+            counts      = Video.Metadata.Counts(
+                            comments = rawdict['video']['count']['comment'],
+                            likes    = rawdict['video']['count']['like'],
+                            mylists  = rawdict['video']['count']['mylist'],
+                            views    = rawdict['video']['count']['view']
+                            ),
+            duration    = rawdict['video']['duration'],
+            postdate    = datetime.datetime.fromisoformat(
+                            rawdict['video']['registeredAt']
+                            ),
+            genre       = Video.Metadata.Genre(
+                            label    = rawdict['genre']['label'],
+                            key      = rawdict['genre']['key']
+                            ),
+            ranking     = Video.Metadata.Ranking(ranking_genre, ranking_tags),
+            series      = Video.Metadata.Series(
+                            seriesid    = rawdict['series']['id'],
+                            title       = rawdict['series']['title'],
+                            description = rawdict['series']['description'],
+                            thumbnail   = rawdict['series']['thumbnailUrl'],
+                            prev_video  = rawdict['series']['video']['prev']['id']
+                                if rawdict['series']['video']['prev'] else None,
+                            next_video  = rawdict['series']['video']['next']['id']
+                                if rawdict['series']['video']['next'] else None,
+                            first_video = rawdict['series']['video']['first']['id']
+                                if rawdict['series']['video']['first'] else None
+                ) if rawdict['series'] else None,
+            thumbnail   = Video.Metadata.Thumbnail(
+                            small_url  = rawdict['video']['thumbnail']['url'],
+                            middle_url = rawdict['video']['thumbnail']['middleUrl'],
+                            large_url  = rawdict['video']['thumbnail']['largeUrl'],
+                            player_url = rawdict['video']['thumbnail']['player'],
+                            ogp_url    = rawdict['video']['thumbnail']['ogp']
+                ),
+            tags        = tags,
+            rawdict     = rawdict
+        )
+
+class User():
+    """ User classes/methods """
+
+    @dataclass
+    class Metadata():
+        """ Meta data """
+        nickname          : str
+        userid            : int
+        description       : User.Metadata.Description
+        user_type         : Literal["Premium", "General"]
+        registered_version: str
+        follow            : int
+        follower          : int
+        user_level        : int
+        user_exp          : int
+        sns               : list[User.Metadata.SNS.User]
+        cover             : Optional[User.Metadata.Cover]
+        icon              : User.Metadata.UserIcon
+        rawdict           : dict
+
+        @dataclass
+        class Description():
+            """ User description """
+            description_html : str
+            description_plain: str
+
+            def __str__(self):
+                return self.description_html
+
+
+        class SNS():
+            """ SNS services / user """
+            @dataclass
+            class Service():
+                """ SNS service (e.g. Twitter) """
+                name: str
+                key : str
+                icon: Annotated[str, "Image URL (PNG)"]
+
+                def __str__(self):
+                    return self.name
+
+            @dataclass
+            class User():
+                """ SNS user """
+                service: User.Metadata.SNS.Service
+                name   : str
+                url    : str
+
+        @dataclass
+        class Cover():
+            """ Cover (Header) image of user """
+            ogp: Annotated[str, "Image URL"]
+            pc : Annotated[str, "Image URL"] #pylint: disable=C0103
+            sp : Annotated[str, "Image URL"] #pylint: disable=C0103
+
+        @dataclass
+        class UserIcon():
+            """ User icon """
+            small: Annotated[str, "Image URL"]
+            large: Annotated[str, "Image URL"]
+
+    @classmethod
+    def get_metadata(cls, userid: int, *, use_cache: bool = False):
+        """ Get user's metadata """
+        watch_url = f"https://www.nicovideo.jp/user/{userid}"
+        try:
+            if use_cache:
+                text = _urllib_request_with_cache(watch_url)
+            else:
+                with urllib.request.urlopen(watch_url) as response:
+                    text = response.read()
+        except urllib.error.HTTPError as exc:
+            if exc.code == 404:
+                raise Error.NicovideoClientError.ContentNotFound("User not found or deleted.")\
+                    from exc
+            else:
+                raise Error.NicovideoClientError.ConnectionError(
+                    f"Unexpected HTTP Error: {exc.code}"
+                ) from exc
+        except urllib.error.URLError as exc:
+            raise Error.NicovideoClientError.ConnectionError("Connection error.") from exc
+
+        soup = bs(text, "html.parser")
+        rawdict = json5.loads(
+            str(soup.select("#js-initial-userpage-data")[0]["data-initial-data"])
+        )["state"]["userDetails"]["userDetails"]["user"]
+
+        return cls.Metadata(
+            nickname           = rawdict["nickname"],
+            userid             = rawdict["id"],
+            description        = cls.Metadata.Description(
+                rawdict["decoratedDescriptionHtml"],
+                rawdict["strippedDescription"]
             ),
-        tags        = tags
-    )
-    if return_rawdict:
-        return _ReturnDataWithRawdict(data=data, rawdict=rawdict)
-    else:
-        return data
+            user_type          = "Premium" if rawdict["isPremium"] else "General",
+            registered_version = rawdict["registeredVersion"],
+            follow             = rawdict["followeeCount"],
+            follower           = rawdict["followerCount"],
+            user_level         = rawdict["userLevel"]["currentLevel"],
+            user_exp           = rawdict["userLevel"]["currentLevelExperience"],
+            sns                = [
+                cls.Metadata.SNS.User(
+                    cls.Metadata.SNS.Service(
+                        account["label"],
+                        account["type"],
+                        account["iconUrl"]
+                    ),
+                    account["screenName"],
+                    account["url"]
+                ) for account in rawdict["sns"]
+            ],
+            cover              = cls.Metadata.Cover(
+                rawdict["coverImage"]["ogpUrl"],
+                rawdict["coverImage"]["pcUrl"],
+                rawdict["coverImage"]["smartphoneUrl"]
+            ) if rawdict["coverImage"] else None,
+            icon               = cls.Metadata.UserIcon(
+                rawdict["icons"]["small"],
+                rawdict["icons"]["large"]
+            ),
+            rawdict            = rawdict
+        )
+
