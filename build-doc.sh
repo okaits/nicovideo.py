77ec6061140252605c3231f1f9fa3d00a347a835
okaits#7534 <okaits@okaits7534.net>
2024-06-13 22:01:04 +0900

    build-doc.shにて、実行するコマンドにpoetryを使い忘れていたのを修正

--- build-doc.sh
+++ build-doc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export SPHINX_APIDOC_OPTIONS=members,show-inheritance
 mkdir -p docs_build/{markdown,output}
-sphinx-apidoc -Fo ./docs_build/markdown nicovideo/
+python3 -m poetry run sphinx-apidoc -Fo ./docs_build/markdown nicovideo/
 echo '
 project = "nicovideo"
 copyright = "2024, okaits#7534"
@@ -13,4 +13,4 @@ extensions.append("sphinx.ext.napoleon")
 extensions.append("sphinx_markdown_builder")
 html_theme = "sphinx_rtd_theme"
 '>>./docs/markdown/conf.py
-sphinx-build -M markdown -a docs_build docs_build/output
+python3 -m poetry sphinx-build -M markdown -a docs_build docs_build/output
