72d4ea2d31ef0ce0cbca76c9cfa62c1098c2349f
okaits#7534 <okaits@okaits7534.net>
2024-08-25 02:04:03 +0900

    バージョンアップ v4.1.1

--- nicovideo/__init__.py
+++ nicovideo/__init__.py
@@ -1,5 +1,5 @@
 """ nicovideo.py: nicovideo API wrapper for Python3. """
 from . import video, errors, user, apirawdicts
 
-__version__ = "4.1.0"
+__version__ = "4.1.1"
 __all__ = ["video", "user", "errors", "apirawdicts"]
